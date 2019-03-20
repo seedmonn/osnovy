1582fa51bf38269269e241031b2ee4278e58351d
seedmonn <47562825+seedmonn@users.noreply.github.com>
2019-03-21 01:06:19 +0300

    Update main.c

--- Lab3/main.c
+++ Lab3/main.c
@@ -3,25 +3,25 @@
 #define NO 0
 void main(void)
 {
-	int c; // òåêóùèé ñèìâîë
-	int prev_c; // ïðåäûäóùèé ñèìâîë
-	int flag; // äâå áóêâû ïîäðÿä
-	int found; // íå ñëîâî
-	int cnt; // ñ÷¸ò÷èê
+	int c; // текущий символ
+	int prev_c; // предыдущий символ
+	int flag; // две буквы подряд
+	int found; // не слово
+	int cnt; // счётчик
 	cnt = 0;
 	flag = NO;
 	found = NO;
-	prev_c = 1; // ïðîñòî ïîòîìó ÷òî 1
+	prev_c = 1; // просто потому что 1
 	printf("Enter some words:\n");
 	while ((c = getchar()) != EOF)
 	{
-		if ((c < 'a' || c > 'z') && (c < 'A' || c > 'Z') && (c != ' ' && c != '.' && c != ',' && c != '\n' && c != '\0' && c != '\t')) // ïðîâåðêà íà íå ñëîâî
+		if ((c < 'a' || c > 'z') && (c < 'A' || c > 'Z') && (c != ' ' && c != '.' && c != ',' && c != '\n' && c != '\0' && c != '\t')) // проверка на не слово
 		{
 			found = YES;
 		}
 		else
 		{
-			if (c == prev_c && (c != ' ' && c != '.' && c != ',' && c != '\n' && c != '\0' && c != '\t')) // óñëîâèå
+			if (c == prev_c && (c != ' ' && c != '.' && c != ',' && c != '\n' && c != '\0' && c != '\t')) // условие
 			{
 				flag = YES;
 			}
